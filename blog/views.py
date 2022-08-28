379eb59188ce1aea66725d524c8c0063747b5436
shahzaibjutt <shahzaibrocks9@gmail.com>
2022-08-28 17:34:51 +0500

    email update

--- blog/views.py
+++ blog/views.py
@@ -13,69 +13,85 @@ from django.core.mail import send_mail
 # Create your views here.
 
 def home(request):
-	postsdict = {
-		'posts':Post.objects.all(),
-	}
-	return render(request, 'blog/home.html', postsdict)
+    postsdict = {
+        'posts': Post.objects.all(),
+    }
+    return render(request, 'blog/home.html', postsdict)
+
 
 class PostListView(ListView):
-	model = Post
-	template_name = 'blog/home.html'
-	context_object_name= 'posts'
-	ordering = '-date_posted'
-	paginate_by= 2
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = '-date_posted'
+    paginate_by = 2
 
 
 class UserPostListView(ListView):
-	model = Post
-	template_name = 'blog/user_posts.html'
-	context_object_name = 'posts'
-	ordering = '-date_posted'
-	paginate_by = 2
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    ordering = '-date_posted'
+    paginate_by = 2
 
-	def get_queryset(self):
-		user = get_object_or_404(User, username=self.kwargs.get('username'))
-		return Post.objects.filter(author=user).order_by('-date_posted')
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 class PostDetailView(DetailView):
-	model = Post
+    model = Post
+
 
 class PostCreateView(LoginRequiredMixin, CreateView):
-	model = Post
-	fields = ['title', 'content']
+    model = Post
+    fields = ['title', 'content']
 
-	def form_valid(self, form):
-		form.instance.author = self.request.user
-		subject = 'Welcome to DataFlair'
-		message = 'Hope you are enjoying your Django Tutorials'
-		recepient = str(self.request.user.email)
-		send_mail(subject, message, EMAIL_HOST_USER, [recepient], fail_silently=False)
-		return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        subject = 'Django Blog Website - New Post Creation Notification'
+        message = 'Your post has been uploaded to the blog'
+        recepient = str(form.instance.author.email)
+        send_mail(subject, message, EMAIL_HOST_USER,
+                  [recepient], fail_silently=False)
+        return super().form_valid(form)
 
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-	model = Post
-	fields = ['title', 'content']
+    model = Post
+    fields = ['title', 'content']
 
-	def form_valid(self, form):
-		form.instance.author = self.request.user
-		return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        subject = 'Django Blog Website - Post Updated Notification'
+        message = f'Your post has been updated.'
+        recepient = str(form.instance.author.email)
+        send_mail(subject, message, EMAIL_HOST_USER,
+                  [recepient], fail_silently=False)
+        return super().form_valid(form)
 
-	def test_func(self):
-		post = self.get_object();
-		if self.request.user == post.author:
-			return True
-		return False
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-	model = Post
-	success_url = '/'
-	def test_func(self):
-		post = self.get_object()
-		if self.request.user == post.author:
-			return True
-		return False
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            subject = 'Django Blog Website - Post Deletion Notification'
+            message = 'Your post has been deleted.'
+            recepient = str(post.author.email)
+            send_mail(subject, message, EMAIL_HOST_USER,
+                      [recepient], fail_silently=False)
+        return True
+        return False
+
 
 def about(request):
-	return HttpResponse('<h1>About</h1>')
+    return HttpResponse('<h1>About</h1>')
